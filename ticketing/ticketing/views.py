1b5e7f138b73f67b572ff3b69caea8c0031f7f00
BESSAI Mohamed Cherif <mohamedcherif.bessai@gmail.com>
2021-03-23 14:20:57 +0100

    third commit

--- ticketing/ticketing/views.py
+++ ticketing/ticketing/views.py
@@ -1,5 +1,23 @@
 
 from django.shortcuts import render
 
+from ticketing.models import Ticket
+import random
+import string
+
+def randomString(stringLength=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+
 def index(request):
     return render(request,"ticketing/index.html")
+
+def submit(request):
+    new_ticket = Ticket(submitter=randomString(), body="Help!")
+    new_ticket.save()
+    return render(request,"ticketing/submit.html")
+
+def tickets(request):
+    all_tickets = Ticket.objects.all()
+    return render(request,"ticketing/tickets.html", {'tickets': all_tickets})
